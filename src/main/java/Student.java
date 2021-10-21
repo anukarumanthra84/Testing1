fb33025c3ffef35624b9e380b26964da0071df32
Anu K P <anu.oct20@gmail.com>
2021-10-21 06:07:42 +0100

    This is my first commit from comandline to git repo

---
+++ src/main/java/Student.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import lombok.*;
+
+import java.io.File;
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @ToString
+    @EqualsAndHashCode
+    @Builder
+    @RequiredArgsConstructor
+    public class Student {
+        private String name;
+        private int rollNumber;
+    }
+
+    class DemoLombok {
+        @SneakyThrows
+        public static void main(String[] args) {
+            System.out.println(new Student("Anu", 1));
+            File f = new File("abc.txt");
+            // FileInputStream fis=new FileInputStream(f);
+            Student s1 = Student.builder().name("Adnan").build();
+            System.out.println(s1);
+        }
+    }
+
+
